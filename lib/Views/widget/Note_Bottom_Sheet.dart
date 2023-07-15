4f50869dcb32db1a8b128d345d00fa9e3d2faca4
Kerolos163 <kokoessa951@gmail.com>
2023-07-15 04:10:42 +0300

    Finish Bottom Sheet

--- lib/Views/widget/Note_Bottom_Sheet.dart
+++ lib/Views/widget/Note_Bottom_Sheet.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/constant.dart';
 
+import 'Custom_Button.dart';
 import 'Custom_TextField.dart';
 
 class AddNote extends StatelessWidget {
@@ -9,23 +11,34 @@ class AddNote extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: const [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            hint: "Title",
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hint: "Content",
-            maxline: 5,
-          )
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: const [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hint: "Title",
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hint: "Content",
+              maxline: 5,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            )
+          ],
+        ),
       ),
     );
   }
 }
+
+
