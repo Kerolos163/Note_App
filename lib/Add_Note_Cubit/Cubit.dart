de0cea99a99eb2463a5444190bc298522ef298f7
Kerolos163 <kokoessa951@gmail.com>
2023-07-16 05:06:35 +0300

    Create add Note State and Cubit

--- lib/Add_Note_Cubit/Cubit.dart
+++ lib/Add_Note_Cubit/Cubit.dart
@@ -4,6 +4,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'State.dart';
 
 class AddNoteAppCubit extends Cubit<AddNoteAppState> {
-  AddNoteAppCubit() : super(InitState());
+  AddNoteAppCubit() : super(InitAddNoteAppState());
   static AddNoteAppCubit get(context) => BlocProvider.of(context);
 }
--- lib/Add_Note_Cubit/State.dart
+++ lib/Add_Note_Cubit/State.dart
@@ -1,3 +1,13 @@
 abstract class AddNoteAppState {}
 
-class InitState extends AddNoteAppState {}
+class InitAddNoteAppState extends AddNoteAppState {}
+
+class AddNoteAppLoading extends AddNoteAppState {}
+
+class AddNoteAppSuccess extends AddNoteAppState {}
+
+class AddNoteAppFailure extends AddNoteAppState {
+  final String error;
+
+  AddNoteAppFailure(this.error);
+}
